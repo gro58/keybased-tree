cc2a7fcdc845e2e9d645479f8738ec8672b4a80c
Rudolf Großmann <rg58@gmx.de>
2022-08-13 12:53:04 +0200

    sureRandomKey, no node object sureRandomKey improves randomKey nodes are defined directly: newNode in createNode, rootNode in createTree

--- main/src/js/keybasedTree.js
+++ main/src/js/keybasedTree.js
@@ -1,30 +1,22 @@
-import {traverseRootToLeafs} from './traverseTree.js';
-/**
- * default node structure
- * @param {string|*} content - the content of the new created node, maybe a string or an object
- * parent and children are represented by their keys or array of keys
- * use of keys is preferred to use of references to parent nodes or child nodes
- * use of keys avoids problems with e.g. circular dependencies if using JSON.stringify
- */
-function node(content) {
-    this.parentKey = null;
-    this.children = [];
-    this.isLeaf = (this.children.length === 0);
-    this.content = content;
-    this.key = 'dummy key'; //overwritten in createTree
-}
+import {
+    traverseRootToLeafs
+} from './traverseTree.js';
 
-// variable tree has to be outside of createTree to have access to createNode
+// variable tree has to be definde outside of createTree to have access to createNode
 var tree = {
-    'root': new node('dummy rootcontent'), //overwritten in createTree
     addNode: function (parentKey, newContent) {
-        return createNode(this, parentKey, newContent);
+        if (parentKey) {
+            return createNode(this, parentKey, newContent);
+        } else {
+            console.error('parentKey undefined or null');
+            return null;
+        }
     },
     /**
      * 
      * @param {function(level, currentNode)} callback - function applied to current node while traversing the tree
      */
-    fromRootToLeaf: function(callback){
+    fromRootToLeaf: function (callback) {
         traverseRootToLeafs(this, callback);
     }
 };
@@ -34,10 +26,19 @@ var tree = {
  * @param {*} rootcontent - the content of the new root node
  * @returns tree object consisting of root node 
  */
-export function createTree(rootcontent){
+export function createTree(rootcontent) {
+    // newTree inherits methods of tree object 
     var newTree = Object.create(tree);
-    newTree.root.content = rootcontent;
-    newTree.root.key = 'root';
+    var rootNode = {
+        key: 'root',
+        parentKey: null,
+        children: [],
+        content: rootcontent,
+        isLeaf: function () {
+            return (this.children.length === 0);
+        }
+    }
+    newTree['root'] = rootNode;
     return newTree;
 }
 
@@ -52,11 +53,14 @@ function createNode(tree, parentKey, newContent) {
     // console.log('add node with content ' + newContent + ' to parent with key ' + parentKey);
     var parent = tree[parentKey];
     if (parent) {
-        var newKey = randomKey(6);
+        var newKey = sureRandomKey(tree, 3);
         var newNode = {
             key: newKey,
             content: newContent,
-            parentKey: parentKey
+            parentKey: parentKey,
+            isLeaf: function () {
+                return (this.children.length === 0);
+            }
         };
         tree[newKey] = newNode;
         parent.children = [...(parent.children || []), newNode.key]
@@ -71,9 +75,33 @@ function createNode(tree, parentKey, newContent) {
 function randomKey(length) {
     var result = '';
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+    // var characters = 'abc'; //for test
     var numOfChars = characters.length;
     for (var i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * numOfChars));
     }
     return result;
+}
+
+function sureRandomKey(tree, length) {
+    const numOfTries = 10; 
+    // const numOfTries = 5; // for test
+    var existingKeys = Object.keys(tree);
+    // console.log('existingKeys', existingKeys);
+    var newKey = null;
+    for (var i = 0; i < numOfTries; i++) {
+        var candidate = randomKey(length);
+        if (existingKeys.indexOf(candidate) === -1) {
+            // candidate is no existing key
+            newKey = candidate;
+            i = numOfTries; // short circuit
+        } else {
+            // no new key found; try again
+            console.log('try number', i, 'not successful:', candidate, 'exists');
+        }
+    }
+    if (newKey === null){
+        throw 'no new key found';
+    }
+    return newKey;
 }
