fd5e6262a07797236bddb4051f6644208b9a50ce
Rudolf Großmann <rg58@gmx.de>
2022-08-24 19:26:42 +0200

    add license.html with links to GitHub more test cases in createLateTestStrings (missing brackets) 	new file:   main/public/license.html 	modified:   main/src/js/createLatexTestStrings.js

--- main/public/build/bundle.js
+++ main/public/build/bundle.js
@@ -359,13 +359,18 @@ var bridge = (function (exports) {
         return tree;
     }
 
-    var version = "0.1.27";
+    var version = "0.1.28";
 
     /**
      * create an array of LaTeX strings with brackets for test purposes
      */
     function createTexStrings(){
         var test = [];
+        test.push('a+3x-5b'); // no brackets
+        test.push('\\left(a+3x-5b'); 
+        test.push('\\left(a+3x-5b)'); 
+        test.push('a+3x-5b\\right]'); 
+        test.push('4+5*[w+t]((3+z)+u)(r-v)');
         test.push('\\int _1^2\\frac{1}{x^2}\\ dx={{result}}');
         test.push('3.14 + \\left[\\left(2a+4b\\right)\\right]');
         test.push('x =\\left[\\left(2a+(4b+c)\\right)\\left(7d-9e\\left(\\frac{z}{u+2(c+3)}\\right)\\right)\\right]');
@@ -373,10 +378,8 @@ var bridge = (function (exports) {
         test.push('u+\\left[2a+\\left(4b+c\\right)\\right][7d-9e\\left(\\frac{z-2}{\\left(u+2\\right)(c+3)}\\right)]');
         test.push('\\left(s+\\left(a+2\\right)(5f-3\\left(w-r\\right))\\left(f-3\\right)-\\left(-s+22\\right)\\right)');
         test.push('3.14+\\left(s+\\left(a+2\\right)[5f-3\\left(w-r\\right)]\\left(f-3\\right)-\\left(-s+22\\right)\\right)');
-        test.push('a+3x-5b'); // no brackets
         test.push('a+3*(x-5b)');
         test.push('4+5*(w+t)[3-[z+u]]');
-        test.push('4+5*[w+t]((3+z)+u)(r-v)');
         return test;
     }
 
