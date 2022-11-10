25056c6eb8e6665b6d3f2dd1331a69d1cb1f9ff8
Rudolf Großmann <rg58@gmx.de>
2022-11-10 13:49:48 +0100

    add link to docs to nav of HTML files 	modified:   main/.npmignore 	new file:   main/.vscode/launch.json.off 	modified:   main/package-lock.json 	modified:   main/package.json 	modified:   main/public/build/keybased-tree-bundle.js 	modified:   main/public/css/keybasedTree.css 	modified:   main/public/docs/createDemoTree.js.html 	modified:   main/public/docs/global.html 	modified:   main/public/docs/index.html 	modified:   main/public/docs/keybasedTree.js.html 	modified:   main/public/docs/traverseTree.js.html 	modified:   main/public/editTree.html 	modified:   main/public/index.html 	modified:   main/public/license.html 	modified:   main/public/testWithAllLeafs.html 	modified:   main/public/viewTree.html

--- main/public/build/keybased-tree-bundle.js
+++ main/public/build/keybased-tree-bundle.js
@@ -336,7 +336,7 @@ var bridge = (function (exports) {
         return tree;
     }
 
-    var version = "0.1.61";
+    var version = "0.1.66";
 
     window.onload = function () {
         console.log('version (from package.json) ', version);
